94c94e51739add79e71ec199d5a4efc46c778c09
Andres Arenas <andresarenas_92@hotmail.com>
2024-03-20 13:38:45 -0600

    Crea funcion para mostrar libros

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const TheFinalEmpire = new Book(
+  'The Final Empire',
+  'Brandon Sanderson',
+  595,
+  true
+);
+const Warbreaker = new Book('Warbreaker', 'Brandon Sanderson', 520, false);
+const TheGraveyardBook = new Book(
+  'The Graveyard Book',
+  'Neil Gaiman',
+  300,
+  true
+);
+
+const books = [TheFinalEmpire, Warbreaker, TheGraveyardBook];
+
+const libraryEl = document.querySelector('.library');
+
+displayBooks();
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -11,4 +31,24 @@ function Book(title, author, pages, read) {
       this.read ? 'readed' : 'not yet readed'
     }`;
   };
+}
+
+function displayBooks() {
+  libraryEl.innerHTML = '';
+  let html = '';
+  for (let book in books) {
+    html += `<div class="grid-el book">
+                <p class="book-title">Title: ${books[book].title}<p>
+                <p class="book-author">Author: ${books[book].author}</p>
+                <p class="book-pages"># Pages: ${books[book].pages}</p>
+                <p class="book-readed">Readed: ${
+                  books[book].read
+                    ? '<span class="check">✔</span>'
+                    : '<span class="uncheck">✖</span>'
+                }</p>
+            </div>
+    `;
+  }
+
+  libraryEl.innerHTML += html;
 }
